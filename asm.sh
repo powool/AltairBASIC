4cfee110a6ceccc3ffaf82cba950fab92241e0f9
Powool <powool@gmail.com>
2026-04-23 22:25:19 -0400

    may be assembling cleanly now

--- asm.sh
+++ asm.sh
@@ -8,7 +8,17 @@
 # make
 # copy binaries somewhere in your $PATH
 
-asl -l -cpu 8080 "$1"
+dir=$(dirname "$1")
+base=$(basename "$1" .asm)
+base=$(basename $base .ASM)
+
+asl -l -cpu 8080 "$1" > $dir/$base.tmp
+if grep '^PASS 2' < $dir/$base.tmp ; then
+	awk 'BEGIN {p = 0;} { if(p) print; } /^PASS 2/ { p = 1;}' $dir/$base.tmp > $dir/$base.lst
+	rm $dir/$base.tmp
+else
+	mv $dir/$base.tmp $dir/$base.lst
+fi
 if [ $? -ne 0 ] ; then
 	echo "Assembly failed. Script terminating."
 	exit 1
@@ -16,9 +26,6 @@ fi
 
 # asl will correctly string either .ASM or .asm,
 # so do that here:
-dir=$(dirname "$1")
-base=$(basename "$1" .asm)
-base=$(basename $base .ASM)
 
-p2hex -F Intel $base.p
+p2hex -F Intel $dir/$base.p
 
